15c2df01a000eee6a763e291df9d8e2634738ee8
abu <abubakarwaheed6346528@gmail.com>
2022-10-20 23:57:48 +0500

    full

--- djangoWebsite/urls.py
+++ djangoWebsite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from webify import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home , name='home'),
+    path('about/', views.about , name='about'),
+    path('services/',views.services , name='ser'),
+    path('workflow/',views.workflow , name='workflow'),
+    path('caseStudy/',views.casestudy , name='case'),
 ]
--- webify/views.py
+++ webify/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+def workflow(request):
+    return render(request, 'workflow.html')
+def casestudy(request):
+    return render(request, 'case_studies.html')
+def about(request):
+    return render(request, 'about.html')
